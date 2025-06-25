1e56dbc356375c7ae9cc3ab00062c04f4c729adc
andrewjamesmoore <andrew.moore@samdesk.io>
2025-06-24 21:36:00 -0600

    feat: add function data to api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from .functions import BUILTIN_FUNCTIONS
 import json
 from pathlib import Path
 
 app = FastAPI()
+
+class PythonFunction(BaseModel):
+    name: str
+    description: str
+    example: str
 
 @app.get("/")
 def root():
@@ -13,3 +21,7 @@ def get_examples():
     data_path = Path(__file__).parent / "data.json"
     with open(data_path) as f:
         return json.load(f)
+
+@app.get("/functions", response_model=List[PythonFunction], tags=["Functions"])
+def get_functions():
+    return BUILTIN_FUNCTIONS
