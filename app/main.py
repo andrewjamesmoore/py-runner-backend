fd044449948ef8dfdc074260986bb3110740b5fb
Ubuntu <ubuntu@ip-172-31-81-63.ec2.internal>
2025-06-27 23:11:01 +0000

    update CORS

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # <-- Your frontend port
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
